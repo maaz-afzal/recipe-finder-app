9e46a416d1032249847a53c8cc4a128aa8a8d317
Maaz Afzal <maazafzal.khattak007@gmail.com>
2026-04-14 04:00:03 -0700

    fix: Github Pages Issue

--- src/main.jsx
+++ src/main.jsx
@@ -2,13 +2,13 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import { FavoritesProvider } from "./context/FavoritesContext.jsx";
 
 createRoot(document.getElementById("root")).render(
-  <BrowserRouter>
+  <HashRouter>
     <FavoritesProvider>
       <App />
     </FavoritesProvider>
-  </BrowserRouter>,
+  </HashRouter>,
 );
